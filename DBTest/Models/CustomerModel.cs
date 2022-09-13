1bc78b3a76109a0d9b9b225fdab2d3011aaea00f
MeltMyGpu <102612447+MeltMyGpu@users.noreply.github.com>
2022-09-13 17:08:47 +0100

    Still in basic exploration

--- DBTest/Models/CustomerModel.cs
+++ DBTest/Models/CustomerModel.cs
@@ -12,7 +12,7 @@ namespace DBTest.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public int PhoneNumber { get; set; }
+        public long PhoneNumber { get; set; }
         public string Address { get; set; }
 
     }
--- DBTest/Program.cs
+++ DBTest/Program.cs
@@ -1,11 +1,25 @@
-﻿
+﻿using System;
+using System.Linq;
+using DBTest.Context;
+using DBTest.Models;
+
+
 namespace Program
 {
     public class Program
     {
         public static void Main()
         {
+            using var Db = new CustomerDbContext();
 
+            Db.Add(new CustomerModel { FirstName = "Kieran", LastName = "Burnett", Address = "CB89TT", PhoneNumber = 07500252300, EmailAddress = "test@test.com", Id=1});
+
+            //Console.WriteLine(Db.Customers.Where(x => Db.Customers. == 1 );
+            var Clients1 = Db.Customers.ToArray();
+            foreach (var client in Clients1)
+            {
+                Console.WriteLine(client.FirstName);
+            }
         }
     
     }
