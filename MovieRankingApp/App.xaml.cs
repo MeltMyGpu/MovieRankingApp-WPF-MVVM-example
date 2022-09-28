0794e3eb56061d2a30a5173c830fa6c254f1e9ed
MeltMyGPU <kieranburnett98@gmail.com>
2022-09-28 17:05:14 +0100

    (DI) Changing workstation
    Added Service host for dependency injection, ensuring Async startup and shutdown for the service;
    Removed start up uri from App.xmal and instead resolve Mainwindow in code behind and open from there;

--- MovieRankingApp/App.xaml.cs
+++ MovieRankingApp/App.xaml.cs
@@ -12,43 +12,77 @@ using Microsoft.EntityFrameworkCore;
 using MovieRankingApp.ViewModels.Interfaces;
 using MovieRankingApp.Models.Interfaces;
 using MovieRankingApp.Models;
-namespace MovieRankingApp
+using Microsoft.Extensions.Hosting;
+using MovieRankingApp.Controllers;
+
+namespace MovieRankingApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+
+    public static IHost? AppHost { get; private set; }
+
+    public App()
     {
-        // ServiceProvider ServiceProvider {get; set;}
-
-        // protected override void OnStartup(StartupEventArgs e)
-        // {
-        //     base.OnStartup(e);
-        //     ServiceProvider = new ServiceCollection()
-        //     .
-        // }
-
-        // start of dependency injection
-        // Still need to implement some form of resolver based on loaded page? 
-        // controller containing all VM's that has propeties returning an instance of service provided MV ?
-        // bind page DataContext to property from this static resource?
-        public App()
-        {
-           this.services = new ServiceCollection();
-           ConfigureServices(services);
-           serviceProvider = services.BuildServiceProvider();
-        }
-        public static ServiceProvider serviceProvider {get; private set;}
-        private IServiceCollection services;
-
-        private void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<MovieRankingDatabaseContext>();
-            services.AddScoped<IMovieRankingDatabaseContext, MovieRankingDatabaseContext>(s => s.GetService<MovieRankingDatabaseContext>());
-            services.AddScoped<IMovieListPageViewModel, MovieListPageViewModel>();
-            services.AddSingleton<MainWindow>();
-        }
-
-
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddTransient<IMovieRankingDatabaseContext, MovieRankingDatabaseContext>();
+                services.AddScoped<IMovieListPageViewModel, MovieListPageViewModel>();
+                services.AddScoped<ViewModelLocator>();
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
     }
+
+    protected override async void OnStartup(StartupEventArgs startupArgs)
+    {
+        await AppHost!.StartAsync(); // ensures the host is started before the app displays
+        var StartUpPage = AppHost.Services.GetRequiredService<MainWindow>(); 
+        // need to override app entry point to an injected version of main window to ensure dependecys are resolved.
+        StartUpPage.Show();
+        
+        base.OnStartup(startupArgs);
+    }
+
+
+    protected override async void OnExit(ExitEventArgs exitArgs)
+    {
+        await AppHost!.StopAsync(); // ensures the host is shutdown before the app closes
+        base.OnExit(exitArgs);
+    }
+    // ServiceProvider ServiceProvider {get; set;}
+
+    // protected override void OnStartup(StartupEventArgs e)
+    // {
+    //     base.OnStartup(e);
+    //     ServiceProvider = new ServiceCollection()
+    //     .
+    // }
+
+    // start of dependency injection
+    // Still need to implement some form of resolver based on loaded page? 
+    // controller containing all VM's that has propeties returning an instance of service provided MV ?
+    // bind page DataContext to property from this static resource?
+    //public App()
+    //{
+    //   this.services = new ServiceCollection();
+    //   ConfigureServices(services);
+    //   serviceProvider = services.BuildServiceProvider();
+    //}
+    //public static ServiceProvider serviceProvider {get; private set;}
+    //private IServiceCollection services;
+
+    //private void ConfigureServices(IServiceCollection services)
+    //{
+    //    services.AddScoped<MovieRankingDatabaseContext>();
+    //    services.AddScoped<IMovieRankingDatabaseContext, MovieRankingDatabaseContext>(s => s.GetService<MovieRankingDatabaseContext>());
+    //    services.AddScoped<IMovieListPageViewModel, MovieListPageViewModel>();
+    //    services.AddSingleton<MainWindow>();
+    //}
+
+
 }
